9de1a2812cfcbb2c8c091456575645995ba6a64f
surya-sivaraj <suryasivarj27@gmail.com>
2025-05-23 21:28:29 +0530

    modify

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -5,27 +5,27 @@ import User from '../models/User.js';
 export const protectRoute = async (req, res, next) => {
     try {
         const token = req.cookies.jwt;
+        // console.log("Token from cookie:", token);
 
         if (!token) {
             return res.status(401).json({ message: 'Unauthorized - No token provided' });
         }
         const decode = jwt.verify(token, process.env.JWT_SECRET_KEY); 
-         if (!decode) {
+        // console.log("Decoded token:", decode);
+
+        if (!decode) {
             return res.status(401).json({ message: 'Unauthorized - Invalid token' });
         }
 
-const user = await User.findById(decode.userId || decode.UserId).select('-password');
- // Exclude password from the user object
+        const user = await User.findById(decode.userId || decode.UserId).select('-password');
         if (!user) {
             return res.status(401).json({ message: 'Unauthorized - User not found' });
         }
 
-        req.user = user; // Attach the user to the request object
+        req.user = user;
         next();
     } catch (error) {
         console.log("Error in auth middleware:", error);
         return res.status(500).json({ message: 'Internal server error' });
-        
-        
     }
 }
